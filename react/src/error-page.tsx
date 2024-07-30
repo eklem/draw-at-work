8c356fb8d41d987037d32110d68dfe50e9bcafbe
Espen Klem <espen.klem@gmail.com>
2024-07-30 13:16:46 +0200

    old error page, w/ two errors.

--- react/src/error-page.tsx
+++ react/src/error-page.tsx
@@ -1,32 +1,18 @@
-import React from 'react';
-import { useRouteError, isRouteErrorResponse } from 'react-router-dom';
+import { useRouteError } from "react-router-dom";
 
-const ErrorPage: React.FC = () => {
-  // you don't need to explicitly set error to `unknown`
+export default function ErrorPage() {
   const error = useRouteError();
-  let errorMessage: string;
-
-  if (isRouteErrorResponse(error)) {
-    // error is type `ErrorResponse`
-    errorMessage =  error.statusText;
-  } else if (error instanceof Error) {
-    errorMessage = error.message;
-  } else if (typeof error === 'string') {
-    errorMessage = error;
-  } else {
-    console.error(error);
-    errorMessage = 'Unknown error';
-  }
+  console.error(error);
 
   return (
-    <div id='error-page' className='flex flex-col gap-8 justify-center items-center h-screen'>
-      <h1 className='text-4xl font-bold'>Oops!</h1>
+    <div id="error-page">
+      <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
-      <p className='text-slate-400'>
-        <i>{errorMessage}</i>
+
+      <h2>Error message:</h2>
+      <p>
+        <i>{error.statusText || error.message}</i>
       </p>
     </div>
   );
-};
-
-export default ErrorPage;
+}
