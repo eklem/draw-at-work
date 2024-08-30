7e7c63e5edf8605b1b04d9538601c878ef96f57d
Espen Klem <espen.klem@gmail.com>
2024-08-30 15:50:31 +0200

    link to idb examples

--- react/src/components.tsx
+++ react/src/components.tsx
@@ -6,8 +6,13 @@ interface checkbox {
 
 function TaskCheckbox(props: checkbox){
   var [done, setDone] = useState(false);
-  // check indexedDB if element exists, if not, write it
+  // check indexedDB if element exists and get it
+  // --> if not, write it
+  // https://github.com/jakearchibald/idb?tab=readme-ov-file#article-store
+  // https://github.com/jakearchibald/idb?tab=readme-ov-file#typescript
   // [code here]
+  console.log(props.id + ' ' + props.value + ' ' + done)
+  
   function handleChange(e: any) {
     // change state in indexedDB
     // [code here]
